41f9eae66763b52eb3d4bf7b5df6f7e747ea85ef
kristiantg <36399668+kristiantg@users.noreply.github.com>
2021-12-16 01:44:10 +0100

    Added logo and cleaned up

--- stads_enhancer.js
+++ stads_enhancer.js
@@ -1,8 +1,6 @@
 let table = document.getElementById('resultTable');
 let tableChildren = table.children;
 let rows = tableChildren[1].rows;
-rows[rows.length-1].remove();
-rows[rows.length-1].remove();
 let sumOfECTS = 0;
 let gradeAverage = 0;
 let gradeAverageWeighted = 0;
@@ -17,11 +15,14 @@ let roundedAverage = Math.round((gradeAverage / entries) * 100 ) / 100;
 
 function calAverage() {
     for (let row of rows) {
+
         let grade = row.children[2].innerText;
-        let ects = row.children[4].innerText;
-        if ((!isNaN(grade)) && parseInt(grade) !== 0) {
-            gradeAverage += parseInt(grade);
-            weightedGradeSum = (parseInt(grade) * parseInt(ects)) / sumOfECTS;
+        let ects = parseFloat(row.children[4].innerText);
+
+        if (!isNaN(grade)) {
+            grade = parseInt(grade);
+            gradeAverage += grade;
+            weightedGradeSum = (grade * ects) / sumOfECTS;
             gradeAverageWeighted += weightedGradeSum;
             entries++;
         }
@@ -32,32 +33,44 @@ function getSumOfECTS() {
     for (let row of rows) {
         let grade = row.children[2].innerText;
 
-        if ((!isNaN(grade)) && parseInt(grade) !== 0) {
-            let ects = row.children[4].innerText;
-            sumOfECTS += parseInt(ects);
+        if (!isNaN(grade)) {
+            let ects = parseFloat(row.children[4].innerText);
+            sumOfECTS += ects;
         }
     }
 }
 
 function removeFailed() {
-    for (let row of rows) {
+    for (let row of rows) { 
         let grade = row.children[2].innerText;
-        if ((!isNaN(grade)) && parseInt(grade) === 0 || grade === 'EB') {
-            row.remove();
+        if(!isNaN(grade)){
+            if(removeFailed && (grade === '00' || grade === 'EB' || grade === '-03')){
+                row.remove();
+            }
         }
     }
 }
 
 function addRow(text, average) {
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0); 
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0); 
     row.insertCell(1);
-    var cell3 = row.insertCell(2); 
+    let cell3 = row.insertCell(2); 
+   
     row.insertCell(3);
     row.insertCell(4);
     cell1.innerHTML = text;
     cell3.innerHTML = average;
+    cell3.className = 'average';
+}
+
+function setStyling(){
+    let resultCells = document.getElementsByClassName('average');
+    for (let cell of resultCells) { 
+        cell.style.textAlign="center";
+    }
 }
 
 addRow("Gennemsnit: ", roundedAverage);
 addRow("Vægtet gennemsnit: ", roundedWeightedAverage);
+setStyling();
